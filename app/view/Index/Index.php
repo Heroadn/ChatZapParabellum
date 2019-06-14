1d7c57aeb4329ec434ec2dc596e5654286410458
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-13 22:59:41 -0300

    Revert "Teste"
    This reverts commit c286a604efa2afd021f1232b0c090edf8e6ab473.

--- app/view/Index/Index.php
+++ app/view/Index/Index.php
@@ -1,24 +1,28 @@
-<form method="post" role="form" action="http://Chat.acid-software.net/Usuario/cadastrar_post"  enctype="multipart/form-data">
-    <div class="form-group">
-        <label for="nome_usuario">Nome do Usuario:</label>
-        <input type="text" class="form-purple" name="nome" id="nome_usuario" aria-describedby="emailHelp" placeholder="Coloque seu nome de usuario aqui...">
-        <small id="nameHelp" class="form-text text-muted">Coloque um nome maior que 5 digitos...</small>
-    </div>
-    <div class="form-group">
-        <label for="email_usuario">Email do Cadastro:</label>
-        <input type="email" class="form-purple" name="email" id="email_usuario" aria-describedby="emailHelp" placeholder="Coloque seu email aqui...">
-        <small id="emailHelp" class="form-text text-muted">Não compartilhamos essa informação com ninguem...</small>
-    </div>
-    <div class="form-group">
-        <label for="usuario_senha">Senha:</label>
-        <input type="password" class="form-purple" name="senha" id="usuario_senha" placeholder="Escreva sua senha aqui...">
-        <small id="nameHelp" class="form-text text-muted">Coloque uma senha maior que 5 digitos...</small>
-    </div>
-</form>
-
-<script src="<?php echo JS . 'integridade_cadastro.js'?>"></script>
+<canvas id = "canvas" width = "640" height = "480"
+        style= "border:1px solid gray; width: 640px; height: 480px;">
+</canvas>
 
 <script>
-    var obj = { '===': "5", '>': "", foto: "NewYork" };
-    verifyFormIntegrity('',obj);
+    $(document).ready(function(){
+        Context.create("canvas");
+
+        var WALL  = "http://www.tigrisgames.com/wall.png";
+        var CRATE = "https://banner2.kisspng.com/20180721/trr/kisspng-super-metroid-metroid-ii-return-of-samus-metroid-super-metroid-5b535a9debc3a7.0035471915321893419657.jpg";
+        var image   = new Sprite(WALL,false);
+        var image2  = new Sprite(CRATE,false);
+        var pattern = new Sprite(CRATE, true);
+        var angle = 0;
+
+        setInterval(function(){
+            Context.context.fillStyle = "#000000";
+            Context.context.fillRect(0,0,800,800);
+
+            image.draw(0,0,64,64);
+            image.draw(0,74,64,64);
+            pattern.draw(160,160,64,64);
+
+            image.rotate(115,160, angle += 4.0);
+            image2.rotate(115,260,-angle/2);
+        },50);
+    });
 </script>
--- app/view/Usuario/Cadastrar.php
+++ app/view/Usuario/Cadastrar.php
@@ -1,36 +1,17 @@
-<div class="text-center" style="margin: 1px auto">
-    <h1 class="p-text"> Cadastrar-se ao ZapChat</h1>
-    <h4 class="p-text">	Converse com seus amigos e paquere, tudo isso de graça! </h4>
-    <p class="p-text" style="font-size: 10px;"><i> *De graça por enquanto >:) ... </i></p>
-
-    <form method="post" role="form" action="http://Chat.acid-software.net/Usuario/cadastrar_post"  enctype="multipart/form-data">
-        <div class="form-group">
-            <label for="nome_usuario">Nome do Usuario:</label>
-            <input type="text" class="form-purple" name="email" id="nome_usuario" aria-describedby="emailHelp" placeholder="Coloque seu nome de usuario aqui...">
-            <small id="nameHelp" class="form-text text-muted">Coloque um nome maior que 5 digitos...</small>
-        </div>
-        <div class="form-group">
-            <label for="email_usuario">Email do Cadastro:</label>
-            <input type="email" class="form-purple" name="email" id="email_usuario" aria-describedby="emailHelp" placeholder="Coloque seu email aqui...">
-            <small id="emailHelp" class="form-text text-muted">Não compartilhamos essa informação com ninguem...</small>
-        </div>
-        <div class="form-group">
-            <label for="usuario_senha">Senha:</label>
-            <input type="password" class="form-purple" name="senha" id="usuario_senha" placeholder="Escreva sua senha aqui...">
-            <small id="nameHelp" class="form-text text-muted">Coloque uma senha maior que 5 digitos...</small>
-
-        </div>
-
+<form class="form-horizontal" method="post" role="form" action="/Usuario/cadastrar_post"  enctype="multipart/form-data">
+    <div class="text-info">Os campos marcados com <i class="fa fa-asterisk"></i> são de preenchimento obrigatório.</div>
+    <br>
+    <div class="form-group">
+        <label for="nome"><span style="color: dodgerblue">*</span>Nome:</label>  <input class="form-control" name="nome" type="text">
+        <label for="senha"><span style="color: dodgerblue">*</span>Senha:</label><input class="form-control" name="senha" type="password">
+        <label for="email"><span style="color: dodgerblue">*</span>Email:</label><input class="form-control" name="email" type="email">
         <label for="foto_perfil"><span style="color: dodgerblue">*</span>Foto:</label><input class="form-control" name="foto_perfil" type="file">
-        
-        <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="terms">
-            <label class="form-check-label" for="terms">Concordo com os termos de uso.</label><br>
-            <a href="https://desciclopedia.org/wiki/Deslivros:Como_fazer_um_pacto_com_o_dem%C3%B4nio"> <i> *terms.zapchat.com.ru* </i> </a>
-        </div>
+        <label for="admin">Admin:</label><input class="form-control" name="admin" type="text">
+    </div>
 
-        <button type="button" onClick="sendNewUser()" class="btn btn-purple purple">Enviar</button>
-    </form>
-
-    <script src="<?php echo JS . 'integridade_cadastro.js'?>"></script>
-</div>
+    <div class="clearfix"></div>
+    <div class="text-right">
+        <a href="" class="btn btn-default" data-dismiss="modal">Cancelar</a>
+        <input type="submit" class="btn btn-primary" value="Salvar">
+    </div>
+</form>
