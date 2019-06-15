d86631cfcfa9ede28e19355811d28c88ae927b68
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-15 11:27:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,8 @@ include_once(TEMPLATE . DIRECTORY_SEPARATOR . 'Config.php');
 set_include_path(get_include_path() .  PATH_SEPARATOR . implode(PATH_SEPARATOR,$modules));
 require ROOT .'vendor/autoload.php';
 
+var_dump('okXD');
+die();
 #Mensagem de erro quanto estiver em ambiente de desenvolvimento
 if(true){ini_set('display_errors',1);error_reporting(-1);}
 date_default_timezone_set('America/Sao_Paulo');
