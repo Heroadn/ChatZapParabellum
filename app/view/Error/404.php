b0b0b376873103f03aa839ba5f54992d13c9f8c3
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-17 15:16:37 -0300

    Update 404.php

--- app/view/Error/404.php
+++ app/view/Error/404.php
@@ -1,3 +1,3 @@
-404 Not found
+<h1>404 Not found</h1>
 <img src="https://media1.tenor.com/images/eabb053c585cee58b42cfba5a898e2f0/tenor.gif?itemid=7838382">
 <iframe src="http://sound.acid-software.net/not_found.html" width="0" height="0" scrolling="no"></iframe>
