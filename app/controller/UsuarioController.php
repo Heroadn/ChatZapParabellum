48ce34d558044ea4f28b9758824d67d08ed9584c
Davi <daviseer@users.noreply.github.com>
2019-06-16 21:29:33 -0300

    Update UsuarioController.php

--- app/controller/UsuarioController.php
+++ app/controller/UsuarioController.php
@@ -164,7 +164,7 @@ class UsuarioController extends Controller
 
         if( !isset($Usuario->nome) || !isset($Usuario->email) || $fromDb !== false)
         {
-            header('Location:' . '/Usuario/Alterar' . $Usuario['id']);
+            header('Location:' . '/Usuario/Alterar/' . $Usuario['id']);
             echo 'erro!';
         }else
         {
