2ddd0490054f544750caeef5c27db38eb488a4fd
Davi <daviseer@users.noreply.github.com>
2019-06-17 01:05:09 -0300

    Update Nav.php

--- app/template/bootstrap/Nav.php
+++ app/template/bootstrap/Nav.php
@@ -2,11 +2,10 @@
     $controller = $this->getController();
     $action = $this->getAction();
 ?>
-
 <nav class="navbar navbar-expand-md navbar-dark purple">
   <div class="container">
     <a class="navbar-brand h1 mb-0 mr-5 menutitle" href="#" style="color: var(--font-light)">
-      <img alt="asdasd" src="resouce/logowhite_zapchat.png" width="40px">
+      <img alt="zapzap" src="../../libs/img/logo_zapchat.png" width="40px">
       &nbsp;&nbsp; ZapChat
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite" style="color: white">
@@ -16,23 +15,23 @@
     <div class="collapse navbar-collapse" id="navbarSite">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/zapzap">Inicio</a>
+          <a class="nav-link" href="http://chat.acid-software.net/">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/zapzap?escolha=2">Login</a>
+          <a class="nav-link" href="/Usuario/Login">Login</a>
 
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/zapzap?escolha=3">Salas</a>
+          <a class="nav-link" href="/Sala/Destaque">Salas</a>
 
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/zapzap?escolha=4">Paginas</a>
+          <a class="nav-link" href="/Sala/">Paginas</a>
 
         </li>
         <li class="nav-item">
           <a class="nav-link" data-toggle="modal" data-target="#editarperfil">Editar Pefil</a>
-          <?php require("modal_editar_perfil.php") ?>
+          <?php require("/Usuario/Alterar") ?>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Sair</a>
