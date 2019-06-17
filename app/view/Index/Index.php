41ef490baeb2d8816f33c0bd8e9d48f9769e71a1
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-17 17:00:37 -0300

    Teste de api com facebook

--- app/view/Index/Index.php
+++ app/view/Index/Index.php
@@ -1,18 +1,3 @@
-<?php
-$fb = new Facebook\Facebook([
-    'app_id' => '750854629068948', // Replace {app-id} with your app id
-    'app_secret' => '9f633bf40ba5075f49f45b8e82420d6c',
-    'default_graph_version' => 'v2.2',
-]);
-
-$helper = $fb->getRedirectLoginHelper();
-
-$permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://chat.acid-software.net/Usuario/fb-callback.php', $permissions);
-
-//echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-?>
-
 <script>
     window.fbAsyncInit = function() {
         FB.init({
@@ -39,12 +24,13 @@ $loginUrl = $helper->getLoginUrl('http://chat.acid-software.net/Usuario/fb-callb
     <div class="col-md-12">
         <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
     </div>
-
+    <br>
+    <br>
     <div class="col-md-12">
         <div
             class="fb-like"
             data-share="true"
-            data-width="450"
+            data-width="272"
             data-show-faces="true">
         </div>
     </div>
