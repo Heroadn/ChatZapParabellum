b98451a10b49e68319b7f0a68d9fa8b22e84ca30
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-17 16:34:47 -0300

    Update Index.php

--- app/view/Index/Index.php
+++ app/view/Index/Index.php
@@ -1,7 +1,7 @@
 <?php
 $fb = new Facebook\Facebook([
     'app_id' => '750854629068948', // Replace {app-id} with your app id
-    'app_secret' => '{9f633bf40ba5075f49f45b8e82420d6c}',
+    'app_secret' => '9f633bf40ba5075f49f45b8e82420d6c',
     'default_graph_version' => 'v2.2',
 ]);
 
@@ -13,10 +13,4 @@ $loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permiss
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 ?>
 
-
 <script src="<?php echo JS . 'integridade_cadastro.js'?>"></script>
-
-<script>
-    var obj = { '===': "5", '>': "", foto: "NewYork" };
-    verifyFormIntegrity('',obj);
-</script>
