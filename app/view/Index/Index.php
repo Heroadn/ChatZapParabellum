40d00d8041fd03e5e5715c8836473854ce406212
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-17 16:42:24 -0300

    Update Index.php

--- app/view/Index/Index.php
+++ app/view/Index/Index.php
@@ -13,4 +13,34 @@ $loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permiss
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 ?>
 
-<script src="<?php echo JS . 'integridade_cadastro.js'?>"></script>
+<button onclick="checkLoginState();">XD</button>
+
+
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '{750854629068948}',
+            cookie     : true,
+            xfbml      : true,
+            version    : '{v3.3}'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    }
+</script>
+
