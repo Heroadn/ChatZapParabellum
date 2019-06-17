85ce54a899529b3cf9dbf0fd3bd5ff52e0940fd4
GabrielMarodin <gabrielmarodin@hotmail.com>
2019-06-17 01:30:31 -0300

    mudado parametro
    trocado parametro de url para input_post que funciona com forms

--- app/view/Sala/Selecionar.php
+++ app/view/Sala/Selecionar.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="col s12 m6 l6">
-        <form action="http://Chat.acid-software.net/Sala/Listar/nome/">
+        <form method="post" action="http://Chat.acid-software.net/Sala/Listar/nome/">
             <input type="search" placeholder="Pesquise salas" name="q">
             <input type="submit">
         </form>
