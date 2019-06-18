222b321ede4ba8f156e4572fe13e111431add3c8
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-18 16:43:14 -0300

    Update SalaController.php

--- app/controller/SalaController.php
+++ app/controller/SalaController.php
@@ -157,7 +157,7 @@ class SalaController extends Controller
 						$this->view->render();		
 					}
 					else{
-					    include(DIRECTORY_SEPARATOR .View . 'Error' . DIRECTORY_SEPARATOR . 'Banido.php');
+					    include(DIRECTORY_SEPARATOR . VIEW . 'Error' . DIRECTORY_SEPARATOR . 'Banido.php');
 					}
 				}
 				else {
