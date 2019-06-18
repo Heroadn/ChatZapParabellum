24971d65e004c11ed640ca630624d20ca3200d97
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-18 16:45:30 -0300

    Update Banido.php

--- app/view/Error/Banido.php
+++ app/view/Error/Banido.php
@@ -1,2 +1,2 @@
 <h1>TÁ BANIDO, VACILÃO!!!!!</h1>
-<iframe width="0" height="0" src="https://sound.acid-software.net/insufficient_permissions.html"></iframe>
+<embed src="https://sound.acid-software.net/sounds/insufficient_permissions.wav" width="0" height="0" ></iframe>
