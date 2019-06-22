e8995ab8b495340610d941f1d8e80b05f334881f
Davi <daviseer@users.noreply.github.com>
2019-06-21 22:55:09 -0300

    Update Nav.php
    test

--- app/template/bootstrap/Nav.php
+++ app/template/bootstrap/Nav.php
@@ -136,6 +136,12 @@
       </ul>
     </div>
   </div>
+
+  <script>
+  $('#exampleModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+  })
+  </script>
   <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
