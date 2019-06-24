0563f5a06b2b697face89248f46c35016afc8f79
GabrielMarodin <gabrielmarodin@hotmail.com>
2019-06-24 19:32:28 -0300

    Create salas.php

---
+++ app/view/Sala/salas.php
@@ -0,0 +1,24 @@
+<div style="height: 500px;">
+	<h2 class="p-text">Ache novas salas para conversar!</h2>
+	
+	<h4 class="p-text">Resultado da sua pesquisa por @pesquisa_por	</h4>
+	<!-- DIV SCROLL - RESULTADO PESQUISA-->
+	<hr/>
+	<div class="result-pesq p-4" style="text-align: left; overflow: auto;">
+		<a href="">
+			<div class="row">
+				<div class="col-sm-12 col-md-4 col-lg-5 col-xl-3">
+					<img class="border-purple" src="img/salas/cute.gif" style="border-radius:50%; max-width: 200px; height:200px">
+				</div>
+				<br>
+				<div class="col-sm-12 col-md-8 col-lg-7 col-xl-9">
+					<h4><strong>@NOME SALA</strong></h4>
+					<p>@Descrição da sala.</p>
+					<p>@tags</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
+<script src="<?php echo JS . 'resultado_salas.js'?>"></script>
+<script src="<?php echo JS . 'mais_de_uma_sala_de_resultado.js'?>"></script>
