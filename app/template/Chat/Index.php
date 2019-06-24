728672532f62e2807885b5c9d8f7159143aacca1
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-24 15:25:39 -0300

    Update Index.php

--- app/template/Chat/Index.php
+++ app/template/Chat/Index.php
@@ -1,27 +1,23 @@
-<?php
-    $this->loadJs();
-    $this->loadCss();
-?>
 
 <div class="darken">
-	<div class="menssages" id="boxChat" style="height: 650px; overflow: auto">
-		<br>
-	</div>
-	<br>
-	<hr>
-	<form>
-		<div class="form-group p-3 text-center">
-			<div class="row">
-				<div class="col-9">
-					<input type="text" class="form-darken-purple" id="mensgtextarea" placeholder="Digite">
-				</div>
-				<div class="col-3">
-					<button type="button" class="btn btn-purple purple" onClick="addNewMsg()">Enviar</button>
-				</div>
-			</div>
-		</div>
-	</form>
+    <div class="menssages" id="boxChat" style="height: 400px;">
+
+    </div>
+
+    <hr>
+    <form id="formChat">
+        <div class="form-group p-3 text-center">
+            <div class="row">
+                <div class="col-9">
+                    <input type="text" class="form-darken-purple" id="mensgtextarea" placeholder="Digite">
+                </div>
+                <div class="col-3">
+                    <button class="btn btn-purple purple">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 
-<script src="<?php echo JS . 'chat.js'?>"></script>
-<script src="<?php echo JS . 'pessoas_online.js'?>"></script>
+<script src="includes/chat.js"></script>
+<script src="includes/pessoas_online.js"></script>
