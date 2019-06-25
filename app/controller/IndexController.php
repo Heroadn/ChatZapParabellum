a53d42f81fe8528baf9d1dcb23bf5275f6a22c69
Davi <daviseer@users.noreply.github.com>
2019-06-24 23:37:50 -0300

    Update IndexController.php

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
      */
     public function Index($id='', $name=''){
         $this->view(['id' =>$id, 'name' =>$name]);
-        $this->view->page_title = 'Index';
+        $this->view->page_title = 'Inicio';
         $this->view->render();
     }
 }
