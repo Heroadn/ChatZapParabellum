ed885931458c42852d808a31b82c202100499303
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-25 12:28:23 -0300

    Update Destaque.php

--- app/view/Sala/Destaque.php
+++ app/view/Sala/Destaque.php
@@ -3,11 +3,11 @@
     /** @var TYPE_NAME $Salas_Destaque */
     echo '<h4>Todas</h4>';
     foreach($Salas_Todas as $value){
-        echo '<a href ="' .'Conversar/'.$value->id. '">'.$value->nome.'</a>' . '<br>';
+        echo '<a href ="' .'https://chat.acid-software.net/Sala/Conversar/'.$value->id. '">'.$value->nome.'</a>' . '<br>';
     }
 
     echo '<h4>Destaque</h4>';
     foreach($Salas_Destaque as $value){
-        echo '<a href ="' .'Conversar/'.$value->id. '">'.$value->nome.'</a>' . '<br>';
+        echo '<a href ="' .'https://chat.acid-software.net/Sala/Conversar/'.$value->id. '">'.$value->nome.'</a>' . '<br>';
     }
 ?>
